74faf4a3187eeaf5492dda3fb8631f06fd479a86
Ezequiel Garcia <elezegarcia@gmail.com>
2012-08-02 12:08:48 -0300

    qemu.sh for serial telnet

--- qemu.sh
+++ qemu.sh
@@ -1,3 +1,5 @@
-#qemu-system-i386 -kernel bzImage -hda probefs.img -append "root=/dev/sda console=ttyS0,38400" -serial file:serial.out
-qemu-system-i386 -kernel bzImage -hda probefs.img -append "root=/dev/sda"
-
+qemu-system-i386 \
+	-kernel bzImage \
+	-hda probefs.img \
+	-append "root=/dev/sda" \
+	-serial telnet:localhost:2222,server,nowait
