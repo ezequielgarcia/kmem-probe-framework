05ba5de306dd5030598a94e881fca61d16c9dd13
Ezequiel Garcia <elezegarcia@gmail.com>
2012-08-10 16:59:54 -0300

    post-process: make trace2account.py output similar to slab_account

--- post-process/trace2account.py
+++ post-process/trace2account.py
@@ -8,7 +8,16 @@ import re
 # map function call_site to total site allocated bytes
 callmap = {}
 
+num_allocs = 0
+num_frees = 0
+num_callers = 0
+total_alloc = 0
+total_req = 0
+total_slack = 0
+
 def init_call(call):
+	global num_callers
+	num_callers += 1
 	callmap[call] = {}
 	callmap[call]['total'] = 0
 	callmap[call]['req'] = 0
@@ -21,6 +30,12 @@ def add_kmalloc_event(call, req, real):
 	if not callmap.has_key(call):
 		init_call(call)
 
+	global num_allocs, total_alloc, total_req, total_slack
+	num_allocs += 1
+	total_alloc += real
+	total_req += req
+	total_slack += (real - req)
+
 	callmap[call]['total'] += real
 	callmap[call]['req'] += req
 	callmap[call]['slack'] += (real - req)
@@ -30,6 +45,8 @@ def add_kfree_event(call):
 	if not callmap.has_key(call):
 		init_call(call)
 
+	global num_frees
+	num_frees += 1
 	callmap[call]['free'] += 1
 
 def main():
@@ -51,14 +68,22 @@ def main():
 		if m:
 			add_kfree_event(m.group(1))
 
-	print 'total (req)\tslack\tcaller\t\talloc/free'
+	print 'total bytes allocated: %8d' % total_alloc
+	print 'total bytes requested: %8d' % total_req
+	print 'slack bytes allocated: %8d' % total_slack
+	print 'number of allocs:      %8d' % num_allocs
+	print 'number of frees:       %8d' % num_frees
+	print 'number of callers:     %8d' % num_callers
+	print ''
+	print '   total    slack      req alloc/free  caller'
+
 	for call in callmap.keys():
-		print '{0} ({1})\t{2}\t{3}\t{4}/{5}'.format(callmap[call]['total'],
-							callmap[call]['req'],
-							callmap[call]['slack'],
-							call,
-							callmap[call]['alloc'],
-							callmap[call]['free'])
-	
+		print('%8d %8d %8d %5d/%-5d %s' % (callmap[call]['total'], 
+			 			   callmap[call]['slack'],
+						   callmap[call]['req'],
+						   callmap[call]['alloc'],
+						   callmap[call]['free'],
+						   call))
+
 if __name__=="__main__":
 	main()
