5181814c1130f7809d52eb78de5a347de795f623
Ezequiel Garcia <elezegarcia@gmail.com>
2012-08-10 12:41:49 -0300

    post-process: add kfree accounting

--- post-process/trace2account.py
+++ post-process/trace2account.py
@@ -5,9 +5,34 @@ import fileinput
 import string
 import re
 
+# map function call_site to total site allocated bytes
+callmap = {}
+
+def init_call(call):
+	callmap[call] = {}
+	callmap[call]['total'] = 0
+	callmap[call]['req'] = 0
+	callmap[call]['slack'] = 0
+	callmap[call]['alloc'] = 0
+	callmap[call]['free'] = 0
+
+
+def add_kmalloc_event(call, req, real):
+	if not callmap.has_key(call):
+		init_call(call)
+
+	callmap[call]['total'] += real
+	callmap[call]['req'] += req
+	callmap[call]['slack'] += (real - req)
+	callmap[call]['alloc'] += 1
+
+def add_kfree_event(call):
+	if not callmap.has_key(call):
+		init_call(call)
+
+	callmap[call]['free'] += 1
+
 def main():
-	# map function call_site to total site allocated bytes
-	callmap = {}
 
 	for line in fileinput.input():
 		# strip trailing \n, if present
@@ -18,25 +43,22 @@ def main():
 		# try to preserve column spacing in the output
 		tmp = line
 		m = re.match(r".*kmalloc.*call_site=([a-zA-Z0-9_+.]+).*bytes_req=([0-9]+)\s*bytes_alloc=([0-9]+)", tmp)
+		if m:
+			add_kmalloc_event(m.group(1), int(m.group(2)), int(m.group(3)))
+
+		m = re.match(r".*kfree.*call_site=([a-zA-Z0-9_+.]+)", tmp)
 
 		if m:
-			call = m.group(1)
-			req = int(m.group(2))
-			alloc = int(m.group(3))
+			add_kfree_event(m.group(1))
 
-			if not callmap.has_key(call):
-				callmap[call] = {}
-				callmap[call]['alloc'] = 0
-				callmap[call]['req'] = 0
-				callmap[call]['slack'] = 0
-
-			callmap[call]['alloc'] += alloc
-			callmap[call]['req'] += req
-			callmap[call]['slack'] += (alloc - req)
-
-	print 'total\treq\tslack\tcaller'
+	print 'total (req)\tslack\tcaller\t\talloc/free'
 	for call in callmap.keys():
-		print '{0}\t{1}\t{2}\t{3}'.format(callmap[call]['alloc'], callmap[call]['req'], callmap[call]['slack'], call)
+		print '{0} ({1})\t{2}\t{3}\t{4}/{5}'.format(callmap[call]['total'],
+							callmap[call]['req'],
+							callmap[call]['slack'],
+							call,
+							callmap[call]['alloc'],
+							callmap[call]['free'])
 	
 if __name__=="__main__":
 	main()
