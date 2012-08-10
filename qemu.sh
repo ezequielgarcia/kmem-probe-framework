c2d29b2280d9889ac4436212422790ec18731e10
Ezequiel Garcia <elezegarcia@gmail.com>
2012-08-10 11:14:47 -0300

    qemu.sh: enable kmem trace from kernel parameters

--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,5 @@
 qemu-system-i386 \
 	-kernel bzImage \
 	-hda probefs.img \
-	-append "root=/dev/sda" \
+	-append "debug ignore_level root=/dev/sda trace_event=kmem:kmalloc,kmem:kmalloc_node,kmem:kfree" \
 	-serial telnet:localhost:2222,server,nowait
