42d2bb7509d4c9ae1dfba4327145885b5927480f
Ezequiel Garcia <elezegarcia@gmail.com>
2012-10-29 07:56:11 -0300

    Remove deprecated scripts
    These have been moved to scripts. Delete them here.
    
    Signed-off-by: Ezequiel Garcia <elezegarcia@gmail.com>

--- clean.sh
+++
@@ -1,4 +0,0 @@
-
-echo "Cleaning previous probefs"
-umount ./probefs &> /dev/null
-rm -rf ./probefs.img ./probefs
--- sources.sh
+++
@@ -1,2 +0,0 @@
-#ª/bin/bash
-wget http://busybox.net/downloads/busybox-1.20.2.tar.bz2
